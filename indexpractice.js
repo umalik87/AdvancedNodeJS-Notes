f4f18d8b81a5ee325c531bc92afa72ca18296f98
umalik87 <umalik87@gmail.com>
2019-10-21 16:38:55 -0400

    New Commit

---
+++ indexpractice.js
@@ -0,0 +1,32 @@
+const cluster = require('cluster')
+
+//Is the file being executed in master mode?
+if (cluster.isMaster) {
+    // Cause index.js to be executed *again* but
+    // in child mode
+    cluster.fork()
+    // cluster.fork()
+    // cluster.fork()
+    // cluster.fork()
+} else {
+    // I'm a child, I'm going to act like a server
+    // and do nothing else
+    const express = require('express')
+    const app = express()
+
+    function doWork(duration) {
+        const start = Date.now()
+        while(Date.now() - start < duration) {}
+    }
+
+    app.get('/', (req, res) => {
+        doWork(5000)
+        res.send('Hi there')
+    })
+
+    app.get('/fast', (req, res) => {
+        res.send('This was fast!')
+    })
+
+    app.listen(3000)
+}
